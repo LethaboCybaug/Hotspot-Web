1f1ae3a23e4490e7dd2d913176ebebf9c3b3fe2b
Lethabo Molefi <molefil@tut.ac.za>
2025-07-03 05:20:40 +0200

    Backend ready to communicate with frontend

--- crime-hotspot/config/cors.php
+++ crime-hotspot/config/cors.php
@@ -6,7 +6,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => ['*'], // or specify React Native device IP
 
     'allowed_origins_patterns' => [],
 
@@ -16,6 +16,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => true,
+    'supports_credentials' => false,
 
 ];
